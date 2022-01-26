c240d5f03f5baeea12d8e495a5bc8900c8d386bf
Toni <garcia.t20@gmail.com>
2022-01-26 17:45:43 +0100

    added logout at index view

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -41,3 +41,23 @@
     <?php } ?>
     </tbody>
 </table>
+
+<div>
+    <div>
+        <div>{{ Auth::user()->name }}</div>
+        <div>{{ Auth::user()->email }}</div>
+    </div>
+
+    <div>
+        <!-- Authentication -->
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <x-responsive-nav-link :href="route('logout')"
+                                   onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                {{ __('Log Out') }}
+            </x-responsive-nav-link>
+        </form>
+    </div>
+</div>
