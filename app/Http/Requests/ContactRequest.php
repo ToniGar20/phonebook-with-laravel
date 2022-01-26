7ea04403edfee9cd6ce0af0c547e5bc36aea87a8
Toni <garcia.t20@gmail.com>
2022-01-26 17:30:42 +0100

    form request for any contact

---
+++ app/Http/Requests/ContactRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first-name' => 'required|between:2,30',
+            'last-name' => 'required|between:2,30',
+            'phone' => 'required|integer|size:9',
+            'phone-type' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'first-name.required' => 'El nombre es requerido',
+            'first-name.between:2,30' => 'El nombre ha de tener entre 2 y 30 caracteres',
+
+            'last-name.required' => 'El apellido es requerido',
+            'last-name.between:2,30' => 'El apellido ha de tener entre 2 y 30 caracteres',
+
+            'phone.required' => 'El teléfono es requerido',
+            'phone.integer' => 'El teléfono ha de tener valor numérico',
+            'phone.size:9' => 'El teléfono ha de tener 9 dígitos',
+
+            'phone-type.required' => 'El tipo de teléfono es requerido'
+
+        ];
+    }
+
+}
