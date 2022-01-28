fbeea3b4d12a5d0c01a5fe019926e7111a6e0713
Toni <garcia.t20@gmail.com>
2022-01-28 16:16:35 +0100

    changing to blade operators

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,10 +1,31 @@
-<form method="get" action="/contacts/create">
-    @csrf
-    @method('GET')
-    <button type="submit">+ Añadir nuevo</button>
-</form>
+@extends('layouts.base-layout')
 
-<table>
+@section('title','Agenda de Contactos')
+
+<h1>Agenda de contactos de {{ Auth::user()->name }}</h1>
+
+<div>
+    <form method="get" action="/contacts/create">
+        @csrf
+        @method('GET')
+        <button type="submit">+ Añadir contacto</button>
+    </form>
+
+    <div>
+        <a href="/">Volver a la Home</a>
+    </div>
+
+    <div>
+        <div><!-- Logout -->
+            <form method="POST" action="/logout">
+                @csrf
+                <button type="submit">Cerrar sesión</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<table class="table table-dark">
     <tbody>
     <tr>
         <th>Nombre</th>
@@ -12,54 +33,30 @@
         <th>Teléfono</th>
         <th>Tipo</th>
         <th>Fecha</th>
-        <th>Editar</th>
-        <th>Borrar</th>
+        <th>Acciones</th>
     </tr>
 
-    <?php
-    foreach ($currentUserContacts as $register){
-    ?>
-    <tr>
-        <td>{{ $register->first_name }}</td>
-        <td>{{ $register->last_name }}</td>
-        <td>{{ $register->phone }}</td>
-        <td>{{ $register->phone_type }}</td>
-        <td><?php
-            $split = explode(" ", $register->updated_at);
-            echo $split[0];
-            ?></td>
-        <td><a href="/contacts/{{ $register->id }}/edit">✏️</a></td>
-        <td>
-            <form method="post" action="/contacts/{{ $register->id }}">
-                @csrf
-                @method('DELETE')
-                <button type="submit">🗑️</button>
-            </form>
-        </td>
+    @foreach ($currentUserContacts as $register)
+        <tr>
+            <td>{{ $register->first_name }}</td>
+            <td>{{ $register->last_name }}</td>
+            <td>{{ $register->phone }}</td>
+            <td>{{ $register->phone_type }}</td>
+            <td><?php
+                $split = explode(" ", $register->updated_at);
+                echo $split[0];
+                ?>
+            </td>
+            <td class="d-flex d-row">
+                <a href="/contacts/{{ $register->id }}/edit">✏️</a>
+                <form method="post" action="/contacts/{{ $register->id }}">
+                    @csrf
+                    @method('DELETE')
+                    <button class="bg-transparent btn-default" type="submit">🗑️</button>
+                </form>
+            </td>
+        </tr>
+    @endforeach
 
-    </tr>
-    <?php } ?>
     </tbody>
 </table>
-
-<div>
-    <div>
-        <div>{{ Auth::user()->name }}</div>
-        <div>{{ Auth::user()->email }}</div>
-    </div>
-
-    <div>
-        <!-- Logout -->
-        <div class="d-flex justify-content-end align-items-center">
-            <form method="POST" action="/logout">
-                @csrf
-                <button type="submit">Cerrar sesión</button>
-            </form>
-        </div>
-    </div>
-
-    <div>
-        <a href="/">Volver a la Home</a>
-    </div>
-
-</div>
