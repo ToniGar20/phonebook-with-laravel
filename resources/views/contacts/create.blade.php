5995f284797a3ee1abdd11393bbeb131210654e3
Toni <garcia.t20@gmail.com>
2022-01-28 17:26:48 +0100

    bootstrap editing

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,33 +1,67 @@
-<h1>Añadir contacto</h1>
+@extends('layouts.base-layout')
 
-@if($errors->any())
-    <div>
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            // {\!! $errors->first('nameofinput', ':messageOferror') !!}
-            @endforeach
-        </ul>
+@section('title','Editar Contacto')
+
+@section('content')
+    <div class="d-flex col-12 bg-light">
+        <div class="vh-100 p-4 bg-dark text-warning w-25" style="border-right: solid 10px blue;">
+            <div class="h-75">
+                <div>
+                    <img width="120rem" height="120rem" alt="phonebook-logo" src="{{ asset('img/phonebook-logo-mini.png') }}" />
+                    <h1 class="h5">Añadir contacto</h1>
+                </div>
+
+                <div class="d-flex flex-column justify-content-end" style="height: 90%;">
+                    <div class="mt-4">
+                        <a class="text-white text-decoration-none" href="/contacts">Volver a contactos</a>
+                    </div>
+                    <div>
+                        <div class="mt-4"><!-- Logout -->
+                            <form method="POST" action="/logout">
+                                @csrf
+                                <button class="bg-warning text-white btn-md rounded-2 px-3" type="submit">Cerrar sesión</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex col-8 justify-content-center align-items-start p-5">
+
+            @if($errors->any())
+                <div>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            // {\!! $errors->first('nameofinput', ':messageOferror') !!}
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form class="d-flex flex-column w-50" method="post" action="/contacts">
+                <!--  Token generation -->
+            @csrf
+            @method('PUT') <!-- Necesario especificarlo porque hay que modificar un registro. Si no se especifica dará un error porque la ruta edit solamente se sooporta con PUT -->
+                <div class="form-group">
+                    <label> Nombre </label>
+                    <input class="form-control" type="text" name="first-name" placeholder="Nombre" required/>
+                </div>
+                <div class="form-group">
+                    <label>Apellido/s</label>
+                    <input class="form-control" type="text" name="last-name" placeholder="Apellido/s" required/>
+                </div>
+                <div class="form-group">
+                    <label>Teléfono</label>
+                    <input class="form-control" type="text" name="phone" placeholder="Teléfono" required/>
+                </div>
+                <div class="form-group">
+                    <label>Tipo</label>
+                    <input class="form-control" type="text" name="phone-type" placeholder="Tipo: Casa, Móvil, Trabajo..."required/>
+                </div>
+                <button class="send-but bg-primary mt-4 text-white btn-md rounded-2 px-3" type="submit" name="send-new">Enviar</button>
+            </form>
+        </div>
     </div>
-@endif
-
-<form class="contact-form" method="post" action="/contacts">
-    <!--  Token generation -->
-    @csrf
-    <label>
-        <input type="text" name="first-name" placeholder="Nombre" required/>
-    </label>
-    <label>
-        <input type="text" name="last-name" placeholder="Apellido/s" required/>
-    </label>
-    <label>
-        <input type="text" name="phone" placeholder="Teléfono" required/>
-    </label>
-    <label>
-        <input type="text" name="phone-type" placeholder="Tipo: Casa, Móvil, Trabajo..." required/>
-    </label>
-    <input class="send-but" type="submit" name="send-new" value="Enviar" required/>
-
-</form>
-
-<a class="home-link" href="/contacts">⬅️Volver</a>
+@endsection
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -2,61 +2,72 @@
 
 @section('title','Agenda de Contactos')
 
-<h1>Agenda de contactos de {{ Auth::user()->name }}</h1>
+@section('content')
 
-<div>
-    <form method="get" action="/contacts/create">
-        @csrf
-        @method('GET')
-        <button type="submit">+ Añadir contacto</button>
-    </form>
+<div class="d-flex col-12 bg-light">
+    <div class="vh-100 p-4 bg-dark text-warning w-25" style="border-right: solid 10px blue;">
+        <div class="h-75">
+            <div>
+                <img width="120rem" height="120rem" alt="phonebook-logo" src="{{ asset('img/phonebook-logo-mini.png') }}" />
+                <h1 class="h5">Agenda de {{ Auth::user()->name }}</h1>
+                <form class="mt-4" method="get" action="/contacts/create">
+                    @csrf
+                    @method('GET')
+                    <button class="bg-primary text-white btn-md rounded-2 px-3" type="submit">+ Añadir contacto</button>
+                </form>
+            </div>
 
-    <div>
-        <a href="/">Volver a la Home</a>
-    </div>
 
-    <div>
-        <div><!-- Logout -->
-            <form method="POST" action="/logout">
-                @csrf
-                <button type="submit">Cerrar sesión</button>
-            </form>
+            <div class="d-flex flex-column justify-content-end" style="height: 90%;">
+                <div class="mt-4">
+                    <a class="text-white text-decoration-none" href="/">Volver a la Home</a>
+                </div>
+                <div>
+                    <div class="mt-4"><!-- Logout -->
+                        <form method="POST" action="/logout">
+                            @csrf
+                            <button class="bg-warning text-white btn-md rounded-2 px-3" type="submit">Cerrar sesión</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 
-<table class="table table-dark">
-    <tbody>
-    <tr>
-        <th>Nombre</th>
-        <th>Apellido/s</th>
-        <th>Teléfono</th>
-        <th>Tipo</th>
-        <th>Fecha</th>
-        <th>Acciones</th>
-    </tr>
-
-    @foreach ($currentUserContacts as $register)
-        <tr>
-            <td>{{ $register->first_name }}</td>
-            <td>{{ $register->last_name }}</td>
-            <td>{{ $register->phone }}</td>
-            <td>{{ $register->phone_type }}</td>
-            <td><?php
-                $split = explode(" ", $register->updated_at);
-                echo $split[0];
-                ?>
-            </td>
-            <td class="d-flex d-row">
-                <a href="/contacts/{{ $register->id }}/edit">✏️</a>
-                <form method="post" action="/contacts/{{ $register->id }}">
-                    @csrf
-                    @method('DELETE')
-                    <button class="bg-transparent btn-default" type="submit">🗑️</button>
-                </form>
-            </td>
-        </tr>
-    @endforeach
-
-    </tbody>
-</table>
+    <div class="d-flex col-8 justify-content-start align-items-start p-5">
+        <table class="table table-dark">
+            <tbody>
+            <tr class="text-primary">
+                <th>Nombre</th>
+                <th>Apellido/s</th>
+                <th>Teléfono</th>
+                <th>Tipo</th>
+                <th>Fecha</th>
+                <th>Acciones</th>
+            </tr>
+            @foreach ($currentUserContacts as $register)
+                <tr>
+                    <td>{{ $register->first_name }}</td>
+                    <td>{{ $register->last_name }}</td>
+                    <td class="text-warning">{{ $register->phone }}</td>
+                    <td>{{ $register->phone_type }}</td>
+                    <td><?php
+                        $split = explode(" ", $register->updated_at);
+                        echo $split[0];
+                        ?>
+                    </td>
+                    <td class="d-flex d-row">
+                        <a href="/contacts/{{ $register->id }}/edit">✏️</a>
+                        <form method="post" action="/contacts/{{ $register->id }}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="bg-transparent btn-default" type="submit">🗑️</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+    </div>
+@endsection
